62adca0cb98dfd6378bcdd7aaa822ff0b6f6b1ce
Kataleko <katalekoweb@gmail.com>
2025-11-27 10:25:48 +0100

    feat: logical operators

---
+++ LogicalOperators.java
@@ -0,0 +1,15 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+        // Logical operators: && (AND), || (OR), ! (NOT)
+
+        int y = 5;
+        int x = 7;
+        int a = 5;
+        int b = 9;
+
+        // boolean result = x > y && a > b;
+        boolean result = a > b;
+
+        System.out.println(!result);
+    }
+}
