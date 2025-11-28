9cbd72204a71b3b67417224d59c3a23466959967
Kataleko <katalekoweb@gmail.com>
2025-11-28 01:09:44 +0100

    feat: switch case

---
+++ Switch.java
@@ -0,0 +1,43 @@
+public class Switch {
+    public static void main(String[] args) {
+        int n = 1;
+
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thuesday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;        
+        //     default:
+        //         break;
+
+        switch (n) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thuesday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> {
+                System.out.println("Invalid number");
+            }
+        }
+    }
+}
