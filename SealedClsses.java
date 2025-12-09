01018638d58d2baf023e2b82aecc0bbb518d64ec
Kataleko <katalekoweb@gmail.com>
2025-12-09 21:23:02 +0100

    sealed classes

---
+++ SealedClsses.java
@@ -0,0 +1,23 @@
+sealed class J permits K,L {
+
+}
+
+final class K extends J {
+
+}
+
+final class L extends J {
+
+}
+
+class M extends J {
+
+}
+
+public class SealedClsses {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ VarKeyWord.java
@@ -0,0 +1,10 @@
+public class VarKeyWord {
+    public static void main(String[] args) {
+        int a = 9;
+        var b = 8;
+
+        int c;
+        var d = 20;
+        var nums = new int[10];
+    }
+}
